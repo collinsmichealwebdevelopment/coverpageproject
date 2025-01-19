b395d8119903cd9cfa2ca27c547d1acaa47b55ab
Collins Aniesodo <colinsanisedo@gmail.com>
2025-01-19 19:53:06 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
-import Card from './containers/cards/card';
+import Card from './containers/cards/Card';
 
 
 const App = () => {
